e319c132e5fa79e38075e4c117773fc88ac40c52
Diego Lima <1diegolimadev@gmail.com>
2026-05-04 03:19:43 -0300

    resolvendo o pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Se for fazer deploy no Github Pages ou similares no futuro, o base url pode ser útil
-  base: './',
+  base: '/LALA-FINACE/',
+
   server: {
     port: 3000,
     open: true
